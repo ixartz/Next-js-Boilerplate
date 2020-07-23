51218b313bd32c0c1fe1d720e6bfaa07827a9236
Rémi Weng <ixartz@users.noreply.github.com>
2020-07-23 22:49:42 +0200

    Add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Main } from '../templates/Main';
+
+const About = () => (
+  <Main>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga recusandae quidem.
+      Quaerat molestiae blanditiis doloremque possimus labore voluptatibus distinctio recusandae
+      autem esse explicabo molestias officia placeat, accusamus aut saepe.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga recusandae quidem.
+      Quaerat molestiae blanditiis doloremque possimus labore voluptatibus distinctio recusandae
+      autem esse explicabo molestias officia placeat, accusamus aut saepe.
+    </p>
+  </Main>
+);
+
+export default About;
