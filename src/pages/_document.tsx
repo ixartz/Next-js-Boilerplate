81f9de32e25204293a47f3f03c11f07a833fb790
Rémi Weng <ixartz@users.noreply.github.com>
2020-09-03 01:37:16 +0200

    Add missing document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
