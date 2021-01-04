17678b149266851e80ca2dab9157adc7540b7058
Rémi Weng <ixartz@users.noreply.github.com>
2021-01-04 19:05:13 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,3 @@
-// Please do not use the array form (like ['tailwindcss', 'postcss-preset-env'])
-// it will create an unexpected error: Invalid PostCSS Plugin found: [0]
-
 module.exports = {
-  plugins: { tailwindcss: {}, autoprefixer: {} },
+  plugins: ['tailwindcss', 'autoprefixer'],
 };
