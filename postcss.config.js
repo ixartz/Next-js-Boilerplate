25c9e01ac8444858090a98f513c6dcd33e35b09d
Rémi Weng <ixartz@users.noreply.github.com>
2021-01-04 16:56:50 +0100

    Update dependencies including Tailwind 2.0

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,5 @@
 // it will create an unexpected error: Invalid PostCSS Plugin found: [0]
 
 module.exports = {
-  plugins: { tailwindcss: {}, 'postcss-preset-env': {} },
+  plugins: { tailwindcss: {}, autoprefixer: {} },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,46 @@
 module.exports = {
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
   theme: {
-    extend: {},
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    extend: {
+      colors: {
+        gray: {
+          100: '#f7fafc',
+          200: '#edf2f7',
+          300: '#e2e8f0',
+          400: '#cbd5e0',
+          500: '#a0aec0',
+          600: '#718096',
+          700: '#4a5568',
+          800: '#2d3748',
+          900: '#1a202c',
+        },
+        blue: {
+          100: '#ebf8ff',
+          200: '#bee3f8',
+          300: '#90cdf4',
+          400: '#63b3ed',
+          500: '#4299e1',
+          600: '#3182ce',
+          700: '#2b6cb0',
+          800: '#2c5282',
+          900: '#2a4365',
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
 };
