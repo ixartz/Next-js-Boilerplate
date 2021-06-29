1c26c8403a3f2a996bb24df88d02e64a95cdfe32
Rémi Weng <ixartz@users.noreply.github.com>
2021-06-29 16:26:44 +0200

    Add explanation why we need a custom _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 import { AppConfig } from '../utils/AppConfig';
 
+// Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
   render() {
     return (
