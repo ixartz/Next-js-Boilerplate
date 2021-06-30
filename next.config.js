4bcb8a94cc5b7fa72582bfd8e9843a3ff7e7d281
Rémi Weng <ixartz@users.noreply.github.com>
2021-06-30 17:10:47 +0200

    Update basePath in Next JS configuration

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: '/demo/Nextjs-Boilerplate',
   reactStrictMode: true,
 });
