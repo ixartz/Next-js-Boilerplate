bdeb58005270f3b17772713508fe0738afccb6b2
Rémi Weng <ixartz@users.noreply.github.com>
2021-07-01 16:07:19 +0200

    Add comment about empty basePath and make it more clear

--- next.config.js
+++ next.config.js
@@ -7,5 +7,8 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
