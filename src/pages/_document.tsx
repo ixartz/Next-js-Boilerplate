6a4591efd8bf6bc288b50b9eeebee256d9d39a43
Blake Ingenthron <blakeingenthron@gmail.com>
2021-07-11 18:22:15 -0500

    see if font gets optimized

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=optional"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Quicksand&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
