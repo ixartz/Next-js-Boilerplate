bf58d50e48ad04c85180faa70af17abe28aad276
Chanh Ly <chanhly.vn@gmail.com>
2021-08-20 16:09:20 +0700

    add http://localhost:3000 to cypress.json

--- cypress/integration/about.spec.tsx
+++ cypress/integration/about.spec.tsx
@@ -1,7 +1,7 @@
 describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/about');
+    cy.visit('/about');
 
     // The new url should include "/about"
     cy.url().should('include', '/about');
