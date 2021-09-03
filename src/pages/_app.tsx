c6184bd976a3aefeeb33ecda15fb63b517844e72
Rémi Weng <ixartz@users.noreply.github.com>
2021-09-03 18:17:24 +0200

    Rename extension.json to extensions.json and disable eslintrc which doesn't work with Next JS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app';
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
   <Component {...pageProps} />
 );
 
