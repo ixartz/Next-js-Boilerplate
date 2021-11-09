a6eed77f5553756de77a19993e6acd0e15ae9389
Tonya <tonya@tokia.dev>
2021-11-09 18:07:07 +0000

    add one pixel of horizontal padding
    adding a pixel of horizontal padding significantly improving the look of web pages from phones.

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -10,7 +10,7 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => (
-  <div className="antialiased w-full text-gray-700">
+  <div className="antialiased w-full text-gray-700 px-1">
     {props.meta}
 
     <div className="max-w-screen-md mx-auto">
