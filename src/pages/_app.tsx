cd70dc4ceebabe7ece3859097596b871d92652f3
Rémi Weng <ixartz@users.noreply.github.com>
2021-12-26 12:38:47 +0100

    Rename main.css to global css following Tailwind official documentation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 
-import '../styles/main.css';
+import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
