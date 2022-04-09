bd7a5d6f820fbf545433acfdf3043084c5536e65
Rémi Weng <ixartz@users.noreply.github.com>
2022-04-09 23:50:13 +0200

    Remove class-methods-use-this rule in eslintrc and only add in _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ import { AppConfig } from '@/utils/AppConfig';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
+  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
       <Html lang={AppConfig.locale}>
