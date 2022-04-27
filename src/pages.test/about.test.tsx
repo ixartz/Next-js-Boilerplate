3652db6af42772108945fef65cf478bb7869ca01
Rémi Weng <ixartz@users.noreply.github.com>
2022-04-27 23:47:37 +0200

    test: move test in pages to a new folder name 'pages.test' to avoid build errors with Next.js

--- src/pages.test/about.test.tsx
+++ src/pages.test/about.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import About from './about';
+import About from '@/pages/about';
 
 // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
 // The mock has been moved to `__mocks__` folder to avoid duplication
--- src/pages.test/index.test.tsx
+++ src/pages.test/index.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 
-import Index from './index';
+import Index from '@/pages/index';
 
 // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
 // The mock has been moved to `__mocks__` folder to avoid duplication
