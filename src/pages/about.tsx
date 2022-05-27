ecbeac75739f6d57eb6eddb93a2efa2acdfb865e
JaeMeDev <jaeme0406@gmail.com>
2022-05-27 10:39:55 +0900

    fix: useTranslation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,7 @@ import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
 const About = () => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation();
 
   return (
     <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
