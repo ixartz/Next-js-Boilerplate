a0791cd615bc77d8d230e1aea6bda4438eaab1f5
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-07 01:35:03 +0200

    test: use cypress testing libray to query/find text

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -3,13 +3,16 @@ describe('Navigation', () => {
     // Start from the index page
     cy.visit('/');
 
-    // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click();
+    // Find a link containing "About" text and click it
+    cy.findByRole('link', { name: 'About' }).click();
 
     // The new url should include "/about"
     cy.url().should('include', '/about');
 
-    // The new page should contain an h1 with "About page"
-    cy.get('p').contains('Lorem');
+    // The new page should contain two "lorem ipsum" paragraphs
+    cy.findAllByText('Lorem ipsum dolor sit amet', { exact: false }).should(
+      'have.length',
+      2
+    );
   });
 });
