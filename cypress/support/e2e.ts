a25ee22387ca00f49fdd7cfd2c14be329853656a
Rémi Weng <ixartz@users.noreply.github.com>
2022-07-08 03:43:15 +0200

    test: fix parsing error with eslint and typescript-eslint parser

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,7 +16,6 @@
 
 // Import commands.js using ES2015 syntax:
 import '@testing-library/cypress/add-commands';
-import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
