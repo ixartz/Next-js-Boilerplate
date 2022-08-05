3fff8bd6a28aded0d9b10f0f94214984cfac0ab4
QL0e <yoonji@ucsb.edu>
2022-08-05 12:20:58 +0900

    feat: played with the headers

--- src/components/articleList/articleList.tsx
+++ src/components/articleList/articleList.tsx
@@ -3,7 +3,7 @@ import ArticleListComponent from './articleListComponent';
 const ArticleList = ({ results }: any) => {
   return (
     <div className="article-list">
-      <div className="article-header header shadow-sm">
+      <div style={{ margin: '40px 0 20px' }}>
         <h2 className="fontresize">Korean Movie, Drama & Entertainment News</h2>
       </div>
       <ArticleListComponent results={results} />
