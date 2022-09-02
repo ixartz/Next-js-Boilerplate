3b46e101b2026ffd5a66c8992d2c97fc40839696
dle519 <david.l@tyk.io>
2022-09-02 13:38:07 -0400

    Changed title

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Starter',
-  title: 'Nextjs Starter',
+  title: 'Tyk Twitter',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
