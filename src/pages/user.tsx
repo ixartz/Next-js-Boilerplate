edb53c65f82f7cacd390717318b04d43032b06bc
Sedky <sedkyaboushamalah@gmail.com>
2022-09-06 10:00:24 -0400

    add todo guideliens

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,8 +1,12 @@
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
 
+// Function to load data here
+
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+    
+    {/* Monica to build below top nav here */}
     <h2>User Profile Page Hello World</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
@@ -16,6 +20,9 @@ const About = () => (
       voluptatibus distinctio recusandae autem esse explicabo molestias officia
       placeat, accusamus aut saepe.
     </p>
+
+    {/* Posts below here eventually */}
+
   </Main>
 );
 
