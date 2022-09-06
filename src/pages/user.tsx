d1cdf98c60ec6bf07f5486946b7f6c14fbcadf60
Sedky <sedkyaboushamalah@gmail.com>
2022-09-06 09:57:46 -0400

    add base user profile page

---
+++ src/pages/user.tsx
@@ -0,0 +1,22 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const About = () => (
+  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+    <h2>User Profile Page Hello World</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
+      voluptatibus distinctio recusandae autem esse explicabo molestias officia
+      placeat, accusamus aut saepe.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
+      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
+      voluptatibus distinctio recusandae autem esse explicabo molestias officia
+      placeat, accusamus aut saepe.
+    </p>
+  </Main>
+);
+
+export default About;
