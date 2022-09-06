23f065d3d5521f829d377a1dc8a3c2b73b28f613
Sedky <sedkyaboushamalah@gmail.com>
2022-09-06 10:10:06 -0400

    Getting rid of the default layout

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -3,17 +3,14 @@ import { Main } from '@/templates/Main';
 
 // Function to load data here
 
-const About = () => (
-  <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
-    
+const User = () => (
+  // <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+
+  < div >
+
     {/* Monica to build below top nav here */}
-    <h2>User Profile Page Hello World</h2>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
+    <h2 className="text-2xl font-bold">Bill Gates</h2>
+    <h2 className="text-l">@BillGates</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
@@ -22,8 +19,9 @@ const About = () => (
     </p>
 
     {/* Posts below here eventually */}
+  </div>
 
-  </Main>
+  // </Main>
 );
 
-export default About;
+export default User;
