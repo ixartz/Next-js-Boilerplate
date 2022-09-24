e04be951a83026d5624ec47c94f5e2c606f4d134
Firaasss <firas@tyk.io>
2022-09-24 14:48:17 -0500

    Forgot to include necessary files

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,7 +1,6 @@
+/* eslint-disable prettier/prettier */
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
-
-// Function to load data here
 
 const User = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
@@ -15,10 +14,29 @@ const User = () => (
         labore voluptatibus distinctio recusandae autem esse explicabo molestias
         officia placeat, accusamus aut saepe.
       </p>
-
-      {/* Posts below here eventually */}
     </div>
   </Main>
 );
 
+interface Posts {
+  name: string;
+  age: string;
+}
+
+function getPosts(): Promise<Posts[]> {
+  return fetch('https://gist.githubusercontent.com/Firaasss/6d94f4e3c5a79beb26b5b3f68dd62c4a/raw/97a5bd562e31e35ec03c6929096f14e353aef161/posts.json')
+    .then((res) => res.json())
+    .then((res) => {
+            // The response has an `any` type, so we need to cast
+            // it to the `User` type, and return it from the promise
+            console.log(res[0].name)
+            return res as Posts[]
+    })
+}
+
+// const result = document.getElementById('result')
+getPosts()
+//         .then(users => {
+//                 result.innerHTML = users.map(u => u.).toString()
+//         })
 export default User;
