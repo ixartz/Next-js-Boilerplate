6af4e99050f6d9b04c0a0ec299266a780b249235
k-kyler <khaiquang690@gmail.com>
2022-10-08 22:19:52 +0700

    feat: re-init project

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'kkyler homepage',
+  title: 'kkyler homepage',
+  description: 'My personal portfolio',
   locale: 'en',
 };
