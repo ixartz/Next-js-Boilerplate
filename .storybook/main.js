09e4eca2489128e20da07be06700c84d22f969f1
Rémi Weng <ixartz@users.noreply.github.com>
2022-12-14 18:03:50 +0100

    feat: default storybook initialization

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    docsPage: true,
+  },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
