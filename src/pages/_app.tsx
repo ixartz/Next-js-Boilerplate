5f612d8cb2581a559adf252ad9cf74bee81e50f5
asepsetiawan9 <zep.setiawan09@gmail.com>
2023-04-11 15:05:01 +0700

    feat(Home Page slicing): slicing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import '../styles/global.css';
 
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
