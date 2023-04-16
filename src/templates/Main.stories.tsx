221ba4c58e2de8053d747c50343d6d23a55dc977
Rémi Weng <ixartz@users.noreply.github.com>
2023-04-16 23:22:14 +0200

    refactor: use satisfy in storybook

--- src/templates/Main.stories.tsx
+++ src/templates/Main.stories.tsx
@@ -4,7 +4,7 @@ import { userEvent, within } from '@storybook/testing-library';
 import { Main } from './Main';
 
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
-const meta: Meta<typeof Main> = {
+const meta = {
   title: 'Example/Main',
   component: Main,
   tags: ['autodocs'],
@@ -12,25 +12,25 @@ const meta: Meta<typeof Main> = {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
     layout: 'fullscreen',
   },
-};
+} satisfies Meta<typeof Main>;
 
 export default meta;
 type Story = StoryObj<typeof Main>;
 
-export const MainWithReactComponent: Story = {
+export const MainWithReactComponent = {
   args: {
     children: <div>Children node</div>,
   },
-};
+} satisfies Story;
 
-export const MainWithString: Story = {
+export const MainWithString = {
   args: {
     children: 'String',
   },
-};
+} satisfies Story;
 
 // More on interaction testing: https://storybook.js.org/docs/7.0/react/writing-tests/interaction-testing
-export const MainWithHomeLink: Story = {
+export const MainWithHomeLink = {
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
     const loginButton = await canvas.getByRole('link', {
@@ -39,4 +39,4 @@ export const MainWithHomeLink: Story = {
 
     await userEvent.click(loginButton);
   },
-};
+} satisfies Story;
