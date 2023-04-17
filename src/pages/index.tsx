c1b95a14fcf42c2c755fddc5b15cc9f1adda0be4
Ross Gustafson <srgustafson8@icloud.com>
2023-04-17 16:57:14 +0100

    test a change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
     <Main
       meta={
         <Meta
-          title="Next.js Boilerplate Presentation"
+          title="Ross Amplify Preso"
           description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
         />
       }
