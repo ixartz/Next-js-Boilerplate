d502869a08a0b1d9025a4ce582651c5353f29d59
Rémi Weng <ixartz@users.noreply.github.com>
2023-04-26 17:01:50 +0200

    feat: remove stories in the coverage from Jest

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,7 @@ const customJestConfig = {
   collectCoverageFrom: [
     './src/**/*.{js,jsx,ts,tsx}',
     '!./src/**/_*.{js,jsx,ts,tsx}',
+    '!./src/**/*.stories.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
   ],
