62d53ee42d5281005f332981e82becb256bbccc0
Rémi Weng <ixartz@users.noreply.github.com>
2023-04-26 16:40:19 +0200

    chore: update latest dependencies

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,8 @@ const customJestConfig = {
     '^@/(.*)$': '<rootDir>/src/$1',
 
     '^@/public/(.*)$': '<rootDir>/public/$1',
+
+    '^__mocks__/(.*)$': '<rootDir>/__mocks__/$1',
   },
   setupFilesAfterEnv: ['./jest.setup.ts'],
   clearMocks: true,
