b82e566fb43d63329ef4507870494e554dea0e6a
Rémi Weng <ixartz@users.noreply.github.com>
2023-08-01 02:06:42 +0200

    feat: add sitemap support app router

--- src/app/new-router/page.tsx
+++ src/app/new-router/page.tsx
@@ -1,4 +1,7 @@
 import type { Metadata } from 'next';
+import Link from 'next/link';
+
+import { Main } from '@/templates/Main';
 
 export const metadata: Metadata = {
   title: 'New Router',
@@ -6,6 +9,25 @@ export const metadata: Metadata = {
     'Incrementally migrate your existing application from pages to app',
 };
 
-export default function Page() {
-  return <h1>Hello, Home page!</h1>;
-}
+const NewRouter = () => (
+  <Main>
+    <p>
+      Moving to the App Router may be the first time using React features that
+      Next.js builds on top of such as Server Components, Suspense, and more.
+      When combined with new Next.js features such as special files and layouts,
+      migration means new concepts, mental models, and behavioral changes to
+      learn.
+    </p>
+    <p>
+      We recommend reducing the combined complexity of these updates by breaking
+      down your migration into smaller steps. The app directory is intentionally
+      designed to work simultaneously with the pages directory to allow for
+      incremental page-by-page migration.{' '}
+      <Link href="https://nextjs.org/docs/pages/building-your-application/upgrading/app-router-migration#migrating-from-pages-to-app">
+        Learn more.
+      </Link>
+    </p>
+  </Main>
+);
+
+export default NewRouter;
---
+++ src/app/portfolio/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from 'next';
+
+import { Main } from '@/templates/Main';
+
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateStaticParams() {
+  return [
+    {
+      slug: '1',
+    },
+  ];
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = params;
+
+  return {
+    title: `Porfolio ${slug}`,
+  };
+}
+
+const PortfolioDetail = ({ params }: Props) => (
+  <Main>
+    <h1 className="capitalize">Porfolio {params.slug}</h1>
+    <p>
+      Designed a series of promotional materials and branding assets for a
+      corporate event. Developed a visually cohesive theme, including a logo,
+      posters, banners, and digital assets. Incorporated the client&apos;s brand
+      identity while adding a fresh and modern touch. Received positive feedback
+      from event attendees and contributed to a successful event with increased
+      attendee participation and brand recognition.
+    </p>
+  </Main>
+);
+
+export const dynamicParams = false;
+
+export default PortfolioDetail;
