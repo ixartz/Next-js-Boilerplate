6b4f8c3105290b704d55640a6d386f4dcebb747b
Rémi Weng <ixartz@users.noreply.github.com>
2023-08-02 15:31:58 +0200

    docs: add sponsors logo in index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,48 @@ const Index = () => {
         <img
           src={`${router.basePath}/assets/images/nextjs-starter-banner.png`}
           alt="Nextjs starter banner"
+          width="600"
         />
       </a>
+      <h2 className="mt-5 text-2xl font-bold">Sponsors</h2>
+      <div className="mt-8 grid grid-cols-3 items-center gap-5 align-middle">
+        <a
+          className="border-none"
+          href="https://clerk.com?utm_source=github&utm_medium=sponsorship&utm_campaign=nextjs-boilerplate"
+        >
+          <img
+            src="assets/images/clerk.png"
+            alt="Clerk – Authentication & User Management for Next.js"
+            width="260"
+          />
+        </a>
+        <a
+          className="border-none"
+          href="https://turso.tech/?utm_source=nextjsstarterbp"
+        >
+          <img
+            src="assets/images/turso.png"
+            alt="SQLite Developer Experience"
+            width="260"
+          />
+        </a>
+        <a
+          className="border-none"
+          href="https://upstash.com/?utm_source=nextjs-boilerplate"
+        >
+          <img
+            src="https://raw.githubusercontent.com/upstash/sponsorship/master/redis.png"
+            alt="Upstash"
+            width="260"
+          />
+        </a>
+        <a className="border-none" href="https://nextlessjs.com">
+          <img
+            src="https://creativedesignsguru.com/assets/images/themes/nextlessjs-github-banner.png"
+            alt="React SaaS Boilerplate Next.js"
+          />
+        </a>
+      </div>
       <h2 className="text-2xl font-bold">
         Boilerplate code for your Nextjs project with Tailwind CSS
       </h2>
