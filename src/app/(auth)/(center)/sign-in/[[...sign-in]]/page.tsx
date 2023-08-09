4489085e8deb0ae1836a3741657f8331af6294ca
Rémi Weng <ixartz@users.noreply.github.com>
2023-08-10 00:08:52 +0200

    feat: add sign in and sign up link in index page

--- src/app/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/(center)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,11 @@
 import { SignIn } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sign in',
+  description:
+    'Effortlessly create an account through our intuitive sign-up process.',
+};
 
 const SignInPage = () => <SignIn />;
 
--- src/app/(auth)/(center)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/(center)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,11 @@
 import { SignUp } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sign up',
+  description:
+    'Seamlessly sign in to your account with our user-friendly login process.',
+};
 
 const SignUpPage = () => <SignUp />;
 
