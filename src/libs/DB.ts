5e2679877a3da64a4cabfc22fdaacebd6abe6789
Rémi Weng <ixartz@users.noreply.github.com>
2023-08-27 14:57:58 +0200

    feat: add schema in drizzle instance and disable migrate in production

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -2,6 +2,8 @@ import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import { migrate } from 'drizzle-orm/libsql/migrator';
 
+import * as schema from '@/models/Schema';
+
 import { Env } from './Env.mjs';
 
 const client = createClient({
@@ -9,6 +11,8 @@ const client = createClient({
   authToken: Env.DATABASE_AUTH_TOKEN,
 });
 
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
 
-await migrate(db, { migrationsFolder: './migrations' });
+if (Env.NODE_ENV !== 'production') {
+  await migrate(db, { migrationsFolder: './migrations' });
+}
