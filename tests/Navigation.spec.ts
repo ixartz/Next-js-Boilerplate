45e087a47029c878b0d778a7c21535657371fa94
Rémi Weng <ixartz@users.noreply.github.com>
2023-10-11 01:34:01 +0200

    test: migrate to Playwright the test 'it should navigate to the about page'

--- tests/Navigation.spec.ts
+++ tests/Navigation.spec.ts
@@ -1,13 +1,23 @@
 import { expect, test } from '@playwright/test';
 
 test.describe('Navigation', () => {
-  test('Static pages', async ({ page }) => {
-    await page.goto('/');
+  test.describe('Static pages', () => {
+    test('should navigate to the about page', async ({ page }) => {
+      await page.goto('/');
 
-    await expect(
-      page.getByRole('heading', {
-        name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
-      }),
-    ).toBeVisible();
+      await expect(
+        page.getByRole('heading', {
+          name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
+        }),
+      ).toBeVisible();
+
+      await page.getByRole('link', { name: 'About' }).click();
+
+      await expect(page).toHaveURL('/about');
+
+      await expect(
+        page.getByText('Lorem ipsum dolor sit amet', { exact: false }),
+      ).toHaveCount(2);
+    });
   });
 });
