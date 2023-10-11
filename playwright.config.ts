61579fa89119ad7162b15f4486a25b62b1090c00
Rémi Weng <ixartz@users.noreply.github.com>
2023-10-11 15:45:41 +0200

    chore: add maxFailures option in Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   // Opt out of parallel tests on CI
   workers: process.env.CI ? 1 : undefined,
+  // Limit the number of failures on CI to save resources
+  maxFailures: process.env.CI ? 10 : undefined,
   // Reporter to use. See https://playwright.dev/docs/test-reporters
   reporter: 'list',
 
