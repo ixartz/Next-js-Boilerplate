9c8ce6b0980ef3009cd18b0115918b214cc2833a
Rémi Weng <ixartz@users.noreply.github.com>
2023-11-10 23:03:47 +0100

    ci: add tests for about page

--- tests/__checks__/Sanity.spec.ts
+++ tests/__checks__/Sanity.spec.ts
@@ -1,11 +1,22 @@
 import { expect, test } from '@playwright/test';
-import assert from 'assert';
 
-const targetUrl = process.env.ENVIRONMENT_URL || 'https://www.checklyhq.com';
+// FIXME: Replace Google.com with your own URL
+const targetUrl = process.env.ENVIRONMENT_URL || 'https://google.com';
 
-test('Visit Checkly HQ page', async ({ page }) => {
-  const response = await page.goto(targetUrl);
+test('should navigate to the about page', async ({ page }) => {
+  await page.goto(targetUrl);
 
-  assert(response !== null, 'response is not null');
-  expect(response.status()).toBeLessThan(400);
+  await expect(
+    page.getByRole('heading', {
+      name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
+    }),
+  ).toBeVisible();
+
+  await page.getByRole('link', { name: 'About' }).click();
+
+  await expect(page).toHaveURL('/about');
+
+  await expect(
+    page.getByText('Lorem ipsum dolor sit amet', { exact: false }),
+  ).toHaveCount(2);
 });
