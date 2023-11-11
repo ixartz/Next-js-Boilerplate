9982a2d94fe7854eefaa754e9f41cf4735a81c86
Rémi Weng <ixartz@users.noreply.github.com>
2023-11-11 22:31:55 +0100

    feat: remove eslint rule customization in VSCode and use min(1) instead of nonempty (deprecated)

--- src/validations/GuestbookValidation.ts
+++ src/validations/GuestbookValidation.ts
@@ -7,8 +7,8 @@ export const GuestbookSchema = z.object({
 
 export const EditGuestbookSchema = z.object({
   id: z.coerce.number(),
-  username: z.string().nonempty(),
-  body: z.string().nonempty(),
+  username: z.string().min(1),
+  body: z.string().min(1),
 });
 
 export const DeleteGuestbookSchema = z.object({
