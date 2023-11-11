61014286f0ddbdd97fdfe016a9cccb0bf9720b0f
Rémi Weng <ixartz@users.noreply.github.com>
2023-11-11 02:36:56 +0100

    ci: add more tests for checkly

--- __checks__/Sanity.spec.ts
+++ __checks__/Sanity.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 // FIXME: Replace Google.com with your own URL
 const targetUrl = process.env.ENVIRONMENT_URL || 'https://google.com';
 
-test('should navigate to the about page', async ({ page }) => {
+test('should display the homepage', async ({ page }) => {
   await page.goto(targetUrl);
 
   await expect(
@@ -11,12 +11,33 @@ test('should navigate to the about page', async ({ page }) => {
       name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
     }),
   ).toBeVisible();
+});
+
+test('should navigate to the about page', async ({ page }) => {
+  await page.goto(targetUrl);
 
   await page.getByRole('link', { name: 'About' }).click();
-
   await expect(page).toHaveURL(/about$/);
 
   await expect(
     page.getByText('Lorem ipsum dolor sit amet', { exact: false }),
   ).toHaveCount(2);
+});
+
+test('should navigate to the portfolio page', async ({ page }) => {
+  await page.goto(targetUrl);
+
+  await page.getByRole('link', { name: 'Portfolio' }).click();
+  await expect(page).toHaveURL(/portfolio$/);
+
+  await expect(page.locator('main').getByRole('link')).toHaveCount(6);
+});
+
+test('should navigate to the blog page', async ({ page }) => {
+  await page.goto(targetUrl);
+
+  await page.getByRole('link', { name: 'Blog' }).click();
+  await expect(page).toHaveURL(/blog$/);
+
+  await expect(page.locator('main').getByRole('link')).toHaveCount(10);
 });
