375e3d1004549a9932a2f673e1b8dde5c03894d7
Rémi Weng <ixartz@users.noreply.github.com>
2023-11-30 19:17:55 +0100

    docs: add Better Stack logo in sponsors component

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -47,6 +47,16 @@ const Sponsors = () => (
           </a>
         </td>
         <td className="border-2 border-gray-300 p-3">
+          <a href="https://betterstack.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate">
+            <Image
+              src="/assets/images/better-stack-dark.png"
+              alt="Better Stack"
+              width={260}
+              height={224}
+            />
+          </a>
+        </td>
+        <td className="border-2 border-gray-300 p-3">
           <a href="https://upstash.com/?utm_source=nextjs-boilerplate">
             <Image
               src="/assets/images/redis.png"
@@ -56,6 +66,8 @@ const Sponsors = () => (
             />
           </a>
         </td>
+      </tr>
+      <tr className="h-56">
         <td className="border-2 border-gray-300 p-3">
           <a href="https://nextlessjs.com">
             <Image
