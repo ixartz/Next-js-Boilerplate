72c1152cbbaf031db2aac47094482d878b569dd4
Rémi Weng <ixartz@users.noreply.github.com>
2023-12-08 16:53:25 +0100

    test: add i18n wrapper for storybook

--- src/templates/BaseTemplate.stories.tsx
+++ src/templates/BaseTemplate.stories.tsx
@@ -1,5 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { userEvent, within } from '@storybook/testing-library';
+import { NextIntlClientProvider } from 'next-intl';
+
+import messages from '@/locales/en.json';
 
 import { BaseTemplate } from './BaseTemplate';
 
@@ -12,6 +15,13 @@ const meta = {
     // More on how to position stories at: https://storybook.js.org/docs/7.0/react/configure/story-layout
     layout: 'fullscreen',
   },
+  decorators: [
+    (Story) => (
+      <NextIntlClientProvider locale="en" messages={messages}>
+        <Story />
+      </NextIntlClientProvider>
+    ),
+  ],
 } satisfies Meta<typeof BaseTemplate>;
 
 export default meta;
