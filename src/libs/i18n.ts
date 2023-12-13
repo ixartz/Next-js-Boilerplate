75aa72bee0bb945ba2dd085adade8aa75ae78091
Rémi Weng <ixartz@users.noreply.github.com>
2023-12-14 00:22:56 +0100

    docs: add comment in Crowdin GitHub Actions and add comments in i18n file

--- src/libs/i18n.ts
+++ src/libs/i18n.ts
@@ -1,5 +1,15 @@
 import { getRequestConfig } from 'next-intl/server';
 
+// NextJS Boilerplate uses Crowdin as the localization software.
+// As a developer, you only need to take care of the English version of messages.
+// Other languages are automatically generated and handled by Crowdin.
+
+// The localisation files are synced with Crowdin using GitHub Actions.
+// By default, there are 3 ways to sync the message files:
+// 1. Automatically sync on push to the `main` branch
+// 2. Run manually the workflow on GitHub Actions
+// 3. Every 24 hours at 5am, the workflow will run automatically
+
 // Using internationalization in Server Components
 export default getRequestConfig(async ({ locale }) => ({
   messages: (await import(`../locales/${locale}.json`)).default,
