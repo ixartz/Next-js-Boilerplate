4bf95038600a28ea3e98e84dabec4df5fd9af609
Rémi Weng <ixartz@users.noreply.github.com>
2023-12-19 18:21:39 +0100

    feat: ignore technical exception throw by React RSC in Sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -7,7 +7,14 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   // dsn: '___DSN___',
 
+  // Enable Spotlight in development
   spotlight: process.env.NODE_ENV === 'development',
+
+  ignoreErrors: [
+    // Workaround for React RSC and Suspense boundaries: https://github.com/amannn/next-intl/issues/614#issuecomment-1862508393
+    // Can be removed once the change is integrated into Sentry SDK.
+    "This is not a real error! It's an implementation detail of `use`",
+  ],
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
