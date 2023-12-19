c3b4d25d3be6a5ceed48f2d365bd14e44ff9b114
Rémi Weng <ixartz@users.noreply.github.com>
2023-12-19 18:26:15 +0100

    feat: in global error get locale in params and set in html lang attribute

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -6,15 +6,17 @@ import { useEffect } from 'react';
 
 export default function GlobalError({
   error,
+  params: { locale },
 }: {
   error: Error & { digest?: string };
+  params: { locale: string };
 }) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
 
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body>
         {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
         <Error statusCode={undefined as any} />
