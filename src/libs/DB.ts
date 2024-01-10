76e98bafbe6779a1e5e21dda85b6f387607bb553
Rémi Weng <ixartz@users.noreply.github.com>
2024-01-10 11:23:36 +0100

    docs: explain why migrate function is not run in production

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -11,7 +11,9 @@ const client = createClient({
 
 export const db = drizzle(client);
 
-// Disable migrate function if using Edge runtime and use `npm run db:migrate` instead
+// Disable migrate function if using Edge runtime and use `npm run db:migrate` instead.
+// Only run migrate in development. Otherwise, migrate will also be run during the build which can cause errors.
+// Migrate during the build can cause errors due to the locked database when multiple migrations are running at the same time.
 if (process.env.NODE_ENV !== 'production') {
   await migrate(db, { migrationsFolder: './migrations' });
 }
