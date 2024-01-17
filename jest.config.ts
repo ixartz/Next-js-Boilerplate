ac547bbb8952c739ac8c00cb2b265d4467c7d4ef
Rémi Weng <ixartz@users.noreply.github.com>
2024-01-17 15:19:17 +0100

    style: add quote in jest config file for filename

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 import nextJest from 'next/jest';
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  // Provide the path to your Next.js app to load `next.config.js` and `.env` files in your test environment
   dir: './',
 });
 
