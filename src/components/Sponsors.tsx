0cdbe3520ddf41dd5bd869d3a81a86a4de189f7e
Rémi Weng <ixartz@users.noreply.github.com>
2024-02-09 15:36:59 +0100

    chore: add Sentry logo in the index page

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -37,6 +37,16 @@ const Sponsors = () => (
       </tr>
       <tr className="h-56">
         <td className="border-2 border-gray-300 p-3">
+          <a href="https://sentry.io/for/nextjs/?utm_source=github&utm_medium=paid-community&utm_campaign=general-fy25q1-nextjs&utm_content=github-banner-nextjsboilerplate-logo">
+            <Image
+              src="/assets/images/sentry-dark.png"
+              alt="Sentry"
+              width={260}
+              height={224}
+            />
+          </a>
+        </td>
+        <td className="border-2 border-gray-300 p-3">
           <a href="https://posthog.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate">
             <Image
               src="https://posthog.com/brand/posthog-logo.svg"
@@ -56,6 +66,8 @@ const Sponsors = () => (
             />
           </a>
         </td>
+      </tr>
+      <tr className="h-56">
         <td className="border-2 border-gray-300 p-3">
           <a href="https://upstash.com/?utm_source=nextjs-boilerplate">
             <Image
@@ -66,8 +78,6 @@ const Sponsors = () => (
             />
           </a>
         </td>
-      </tr>
-      <tr className="h-56">
         <td className="border-2 border-gray-300 p-3">
           <a href="https://www.checklyhq.com/?utm_source=github&utm_medium=sponsorship&utm_campaign=next-js-boilerplate">
             <Image
