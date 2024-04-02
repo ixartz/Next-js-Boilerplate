db94f31615cd5ffcc3739ab56572646f7ce1f177
Rémi Weng <ixartz@users.noreply.github.com>
2024-04-03 00:14:11 +0200

    feat: run migration only in development and eslint-disable need to be at the top

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: process.env.CI ? 'npm run start' : 'npm run dev',
+    command: process.env.CI ? 'npm run start' : 'npm run dev:next',
     url: baseURL,
     timeout: 2 * 60 * 1000,
     reuseExistingServer: !process.env.CI,
--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -14,6 +14,6 @@ export const db = drizzle(client);
 // Disable migrate function if using Edge runtime and use `npm run db:migrate` instead.
 // Only run migrate in development. Otherwise, migrate will also be run during the build which can cause errors.
 // Migrate during the build can cause errors due to the locked database when multiple migrations are running at the same time.
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'development') {
   await migrate(db, { migrationsFolder: './migrations' });
 }
