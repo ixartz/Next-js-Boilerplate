2a681244f834b6ea55bcd5cd3105f8b4a9df4a05
Rémi Weng <ixartz@users.noreply.github.com>
2024-05-17 13:54:16 +0200

    feat: vscode jest open test result view on test fails and add unauthenticatedUrl in clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,13 +19,24 @@ export default function middleware(
   request: NextRequest,
   event: NextFetchEvent,
 ) {
+  // Run Clerk middleware only when it's necessary
   if (
     request.nextUrl.pathname.includes('/sign-in') ||
     request.nextUrl.pathname.includes('/sign-up') ||
     isProtectedRoute(request)
   ) {
     return clerkMiddleware((auth, req) => {
-      if (isProtectedRoute(req)) auth().protect();
+      if (isProtectedRoute(req)) {
+        const locale =
+          req.nextUrl.pathname.match(/(\/.*)\/dashboard/)?.at(1) ?? '';
+
+        const signInUrl = new URL(`${locale}/sign-in`, req.url);
+
+        auth().protect({
+          // `unauthenticatedUrl` is needed to avoid error: "Unable to find `next-intl` locale because the middleware didn't run on this request"
+          unauthenticatedUrl: signInUrl.toString(),
+        });
+      }
 
       return intlMiddleware(req);
     })(request, event);
