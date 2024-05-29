62aa6786117637e6b76c97f6c98f7ca6e8c343b0
Rémi Weng <ixartz@users.noreply.github.com>
2024-05-29 12:12:41 +0200

    feat: update to Drizzle kit 0.21 version, no need to have dialect in command line

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,10 @@ import type { Config } from 'drizzle-kit';
 export default {
   out: './migrations',
   schema: './src/models/Schema.ts',
-  driver: 'libsql',
+  dialect: 'sqlite',
   dbCredentials: {
     url: process.env.DATABASE_URL ?? '',
   },
+  verbose: true,
+  strict: true,
 } satisfies Config;
