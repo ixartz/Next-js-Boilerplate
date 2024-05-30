ec3ee86b89a5af6e716f8a49e0cd4f449273cb7b
Rémi Weng <ixartz@users.noreply.github.com>
2024-05-30 22:44:34 +0200

    feat: add turso driver in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,7 @@ export default {
   out: './migrations',
   schema: './src/models/Schema.ts',
   dialect: 'sqlite',
+  driver: 'turso',
   dbCredentials: {
     url: process.env.DATABASE_URL ?? '',
     authToken: process.env.DATABASE_AUTH_TOKEN ?? '',
