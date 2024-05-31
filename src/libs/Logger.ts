76bb4002f741004bfbbb888b61980b0fcf32cd02
Rémi Weng <ixartz@users.noreply.github.com>
2024-05-31 16:39:17 +0200

    feat: use logtail with stream

--- src/libs/Logger.ts
+++ src/libs/Logger.ts
@@ -1,3 +1,4 @@
+import logtail from '@logtail/pino';
 import pino, { type DestinationStream } from 'pino';
 import pretty from 'pino-pretty';
 
@@ -6,8 +7,14 @@ import { Env } from './Env';
 let stream: DestinationStream;
 
 if (Env.LOGTAIL_SOURCE_TOKEN) {
-  stream = pretty({
-    colorize: true,
+  // Use in production
+  stream = await logtail({
+    sourceToken: Env.LOGTAIL_SOURCE_TOKEN,
+    options: {
+      sendLogsToConsoleOutput: true,
+      sendLogsToBetterStack: true,
+      contextObjectMaxDepth: 0,
+    },
   });
 } else {
   stream = pretty({
