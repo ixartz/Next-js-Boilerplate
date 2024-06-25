80d369a9d374cb5557356d9ea794719e3a1f59d5
Rémi Weng <ixartz@users.noreply.github.com>
2024-06-25 03:46:45 +0200

    feat: add updateAt and createdAt attribute in guestbook

--- migrations/0000_init_db.sql
+++ migrations/0000_init_db.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS "guestbook" (
 	"id" serial NOT NULL,
 	"username" text NOT NULL,
-	"body" text NOT NULL
+	"body" text NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
 );
--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -1,7 +1,12 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const guestbookSchema = pgTable('guestbook', {
   id: serial('id'),
   username: text('username').notNull(),
   body: text('body').notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
 });
