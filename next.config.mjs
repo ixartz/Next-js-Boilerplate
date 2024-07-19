bed334b5af10863f4d94c3d91e79ed75a775abbf
Rémi Weng <ixartz@users.noreply.github.com>
2024-07-19 23:43:54 +0200

    chore: update Sentry to version 8

--- next.config.mjs
+++ next.config.mjs
@@ -33,24 +33,23 @@ export default withSentryConfig(
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
     // FIXME: Add your Sentry organization and project names
     org: 'nextjs-boilerplate-org',
     project: 'nextjs-boilerplate',
-  },
-  {
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
     // For all available options, see:
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
-
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
     tunnelRoute: '/monitoring',
 
     // Hides source maps from generated client bundles
@@ -59,7 +58,7 @@ export default withSentryConfig(
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
 
-    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
     // See the following for more information:
     // https://docs.sentry.io/product/crons/
     // https://vercel.com/docs/cron-jobs
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -12,12 +12,6 @@ Sentry.init({
   // Enable Spotlight in development
   spotlight: process.env.NODE_ENV === 'development',
 
-  ignoreErrors: [
-    // Workaround for React RSC and Suspense boundaries: https://github.com/amannn/next-intl/issues/614#issuecomment-1862508393
-    // Can be removed once the change is integrated into Sentry SDK.
-    "This is not a real error! It's an implementation detail of `use`",
-  ],
-
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
