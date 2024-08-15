8f31536f29ce6599fb9ccbfae8dc176080a9215d
Rémi Weng <ixartz@users.noreply.github.com>
2024-08-16 01:26:50 +0200

    feat: use new ESLint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import jestDom from 'eslint-plugin-jest-dom';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import playwright from 'eslint-plugin-playwright';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tailwind from 'eslint-plugin-tailwindcss';
+import testingLibrary from 'eslint-plugin-testing-library';
+
+export default antfu({
+  react: true,
+  typescript: true,
+
+  lessOpinionated: true,
+  isInEditor: false,
+
+  stylistic: {
+    semi: true,
+  },
+
+  formatters: {
+    css: true,
+  },
+
+  ignores: [
+    'next-env.d.ts',
+    'migrations/**/*',
+  ],
+}, ...tailwind.configs['flat/recommended'], jsxA11y.flatConfigs.recommended, {
+  plugins: {
+    '@next/next': nextPlugin,
+  },
+  rules: {
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs['core-web-vitals'].rules,
+  },
+}, {
+  plugins: {
+    'simple-import-sort': simpleImportSort,
+  },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+}, {
+  files: [
+    '**/*.test.ts',
+    '**/*.test.tsx',
+  ],
+  ...testingLibrary.configs['flat/react'],
+  ...jestDom.configs['flat/recommended'],
+}, {
+  files: [
+    '**/*.spec.ts',
+  ],
+  ...playwright.configs['flat/recommended'],
+}, {
+  rules: {
+    'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+    'test/index': 'error',
+    'test/prefer-lowercase-title': 'off',
+  },
+});
--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  '**/*.ts?(x)': () => 'npm run check-types',
-  '*.{json,yaml}': ['prettier --write'],
-};
