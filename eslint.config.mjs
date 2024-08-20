544c54197ee74930c01b0d28cfd250c6bc384080
Remi Wg <ixartz@users.noreply.github.com>
2024-08-20 15:55:02 +0200

    fix: resolve lint sort conflicts with simple-import-sort

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -55,6 +55,7 @@ export default antfu({
   ...playwright.configs['flat/recommended'],
 }, {
   rules: {
+    'import/order': 'off', // Avoid conflicts with `simple-import-sort` plugin
     'sort-imports': 'off', // Avoid conflicts with `simple-import-sort` plugin
     'style/brace-style': ['error', '1tbs'], // Use the default brace style
     'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
