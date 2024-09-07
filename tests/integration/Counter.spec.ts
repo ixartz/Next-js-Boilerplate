924f4e03707b0c0485e0f13a7ce8d81cb5390395
Rémi Weng <ixartz@users.noreply.github.com>
2024-09-07 15:02:36 +0200

    test: add counter tests with incorrect input

--- tests/integration/Counter.spec.ts
+++ tests/integration/Counter.spec.ts
@@ -2,7 +2,43 @@ import { expect, test } from '@playwright/test';
 
 test.describe('Counter', () => {
   test.describe('Basic database operations', () => {
-    test('should increment the counter', async ({
+    test('shouldn\'t increment the counter with an invalid input', async ({
+      request,
+    }) => {
+      const counter = await request.put('/api/counter', {
+        data: {
+          increment: 'incorrect',
+        },
+      });
+
+      expect(counter.status()).toBe(422);
+    });
+
+    test('shouldn\'t increment the counter with a negative number', async ({
+      request,
+    }) => {
+      const counter = await request.put('/api/counter', {
+        data: {
+          increment: -1,
+        },
+      });
+
+      expect(counter.status()).toBe(422);
+    });
+
+    test('shouldn\'t increment the counter with a number greater than 3', async ({
+      request,
+    }) => {
+      const counter = await request.put('/api/counter', {
+        data: {
+          increment: 5,
+        },
+      });
+
+      expect(counter.status()).toBe(422);
+    });
+
+    test('should increment the counter and update the counter correctly', async ({
       request,
     }) => {
       let counter = await request.put('/api/counter', {
