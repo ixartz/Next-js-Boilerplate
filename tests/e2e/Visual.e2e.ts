4806216cd9bde02bcee94015b58ddcf7844a0041
Rémi Weng <ixartz@users.noreply.github.com>
2024-09-09 01:03:37 +0200

    test: add visual testing for portfolio, sign up and sign in page

--- tests/e2e/Visual.e2e.ts
+++ tests/e2e/Visual.e2e.ts
@@ -26,5 +26,61 @@ test.describe('Visual testing', () => {
 
       await percySnapshot(page, 'About');
     });
+
+    test('should take screenshot of the counter page', async ({ page }) => {
+      await page.goto('/counter');
+
+      await expect(
+        page.getByRole('link', {
+          name: 'Counter',
+        }),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Counter');
+    });
+
+    test('should take screenshot of the portfolio page and one details page', async ({ page }) => {
+      await page.goto('/portfolio');
+
+      await expect(
+        page.getByRole('link', {
+          name: 'Portfolio',
+        }),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Portfolio');
+
+      await page.getByRole('link', {
+        name: 'Portfolio 2',
+      }).click();
+
+      await expect(
+        page.getByRole('link', {
+          name: 'Portfolio',
+        }),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Portfolio details');
+    });
+
+    test('Sign up', async ({ page }) => {
+      await page.goto('/sign-up');
+
+      await expect(
+        page.getByText('Create your account'),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Sign up');
+    });
+
+    test('Sign in', async ({ page }) => {
+      await page.goto('/sign-in');
+
+      await expect(
+        page.getByText('Sign in to'),
+      ).toBeVisible();
+
+      await percySnapshot(page, 'Sign in');
+    });
   });
 });
