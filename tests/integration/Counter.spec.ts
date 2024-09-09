7d3b32d2481049e0aa3edaa387b4fff33adb3db6
Rémi Weng <ixartz@users.noreply.github.com>
2024-09-09 23:41:42 +0200

    test: remove increment counter integration test to avoid to conflict with e2e tests

--- tests/integration/Counter.spec.ts
+++ tests/integration/Counter.spec.ts
@@ -37,41 +37,5 @@ test.describe('Counter', () => {
 
       expect(counter.status()).toBe(422);
     });
-
-    test('should increment the counter and update the counter correctly', async ({
-      request,
-    }) => {
-      let counter = await request.put('/api/counter', {
-        data: {
-          increment: 1,
-        },
-      });
-      let counterJson = await counter.json();
-
-      expect(counter.status()).toBe(200);
-
-      // Save the current count
-      const count = counterJson.count;
-
-      counter = await request.put('/api/counter', {
-        data: {
-          increment: 2,
-        },
-      });
-      counterJson = await counter.json();
-
-      expect(counter.status()).toBe(200);
-      expect(counterJson.count).toEqual(count + 2);
-
-      counter = await request.put('/api/counter', {
-        data: {
-          increment: 1,
-        },
-      });
-      counterJson = await counter.json();
-
-      expect(counter.status()).toBe(200);
-      expect(counterJson.count).toEqual(count + 3);
-    });
   });
 });
