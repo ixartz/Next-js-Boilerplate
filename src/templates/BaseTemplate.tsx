c42821fda9dad4eb38cf34eea594e79596aa7d15
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-01 17:11:53 +0200

    refactor: simplify footer text

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -38,14 +38,17 @@ export const BaseTemplate = (props: {
         <main>{props.children}</main>
 
         <footer className="border-t border-gray-300 py-8 text-center text-sm">
-          {`© Copyright ${new Date().getFullYear()} ${AppConfig.name}. ${t('made_with')} `}
-          <a
-            href="https://creativedesignsguru.com"
-            className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-          >
-            CreativeDesignsGuru
-          </a>
-          .
+          {`© Copyright ${new Date().getFullYear()} ${AppConfig.name}. `}
+          {t.rich('made_with', {
+            author: () => (
+              <a
+                href="https://creativedesignsguru.com"
+                className="text-blue-700 hover:border-b-2 hover:border-blue-700"
+              >
+                CreativeDesignsGuru
+              </a>
+            ),
+          })}
           {/*
            * PLEASE READ THIS SECTION
            * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
