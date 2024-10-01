f7779146e954a86c311577c24e2e80a846c36d46
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-01 03:43:49 +0200

    fix: simply the counter Route handler by removing try-catch block and improve readability in form

--- src/app/[locale]/(unauth)/api/counter/route.ts
+++ src/app/[locale]/(unauth)/api/counter/route.ts
@@ -21,23 +21,17 @@ export const PUT = async (request: Request) => {
   // The default value is 0 when there is no `x-e2e-random-id` header
   const id = Number(headers().get('x-e2e-random-id')) ?? 0;
 
-  try {
-    const count = await db
-      .insert(counterSchema)
-      .values({ id, count: parse.data.increment })
-      .onConflictDoUpdate({
-        target: counterSchema.id,
-        set: { count: sql`${counterSchema.count} + ${parse.data.increment}` },
-      }).returning();
+  const count = await db
+    .insert(counterSchema)
+    .values({ id, count: parse.data.increment })
+    .onConflictDoUpdate({
+      target: counterSchema.id,
+      set: { count: sql`${counterSchema.count} + ${parse.data.increment}` },
+    }).returning();
 
-    logger.info('Counter has been incremented');
+  logger.info('Counter has been incremented');
 
-    return NextResponse.json({
-      count: count[0]?.count,
-    });
-  } catch (error) {
-    logger.error(error, 'An error occurred while incrementing the counter');
-
-    return NextResponse.json({}, { status: 500 });
-  }
+  return NextResponse.json({
+    count: count[0]?.count,
+  });
 };
