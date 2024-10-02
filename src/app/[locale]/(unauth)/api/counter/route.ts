aabb40a689d11f6776af7b769adeb00fa786c882
Rémi Weng <ixartz@users.noreply.github.com>
2024-10-02 16:38:52 +0200

    fix: try with dispose

--- src/app/[locale]/(unauth)/api/counter/route.ts
+++ src/app/[locale]/(unauth)/api/counter/route.ts
@@ -1,3 +1,4 @@
+import { dispose } from '@logtape/logtape';
 import { sql } from 'drizzle-orm';
 import { headers } from 'next/headers';
 import { NextResponse } from 'next/server';
@@ -30,6 +31,7 @@ export const PUT = async (request: Request) => {
     }).returning();
 
   logger.info('Counter has been incremented');
+  await dispose();
 
   return NextResponse.json({
     count: count[0]?.count,
