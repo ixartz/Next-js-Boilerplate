4f144d03aba073e879a30a477c563cb6576bc3b4
Rémi Weng <ixartz@users.noreply.github.com>
2024-11-14 08:15:09 +0100

    fix: add onRequestError in instrumentations file to capture nested rsc errors

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,7 @@
 import * as Sentry from '@sentry/nextjs';
 
+export const onRequestError = Sentry.captureRequestError;
+
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     // Node.js Sentry configuration
