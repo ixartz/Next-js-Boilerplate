229ba7e6941e9415e64e6957d1a3163d0fd26082
Rémi Weng <ixartz@users.noreply.github.com>
2024-11-15 03:06:04 +0100

    revert: add back Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+  core: {
+    disableTelemetry: true,
+  },
+};
+
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from '@storybook/react';
+
+import '../src/styles/global.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+};
+
+export default preview;
