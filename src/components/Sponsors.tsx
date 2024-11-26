8e178b1ae0d85a068941c84d6837779d97b82faf
Rémi Weng <ixartz@users.noreply.github.com>
2024-11-26 23:57:11 +0100

    chore: reorganize logos in sponsors components

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -99,18 +99,6 @@ export const Sponsors = () => (
             />
           </a>
         </td>
-      </tr>
-      <tr className="h-56">
-        <td className="border-2 border-gray-300 p-3">
-          <a href="https://launch.arcjet.com/Q6eLbRE">
-            <Image
-              src="/assets/images/arcjet-light.svg"
-              alt="Arcjet"
-              width={260}
-              height={224}
-            />
-          </a>
-        </td>
         <td className="border-2 border-gray-300 p-3">
           <a href="https://nextjs-boilerplate.com/pro-saas-starter-kit">
             <Image
