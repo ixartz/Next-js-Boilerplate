5ef2fc52f0cc3d7b6a8754832f4dda9127098df5
Rémi Weng <ixartz@users.noreply.github.com>
2024-12-20 15:25:54 +0100

    fix: add new message in hello component

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -12,6 +12,18 @@ export const Hello = async () => {
         {`👋 `}
         {t('hello_message', { email: user?.emailAddresses[0]?.emailAddress })}
       </p>
+      <p>
+        {t.rich('alternative_message', {
+          url: () => (
+            <a
+              className="text-blue-700 hover:border-b-2 hover:border-blue-700"
+              href="https://nextjs-boilerplate.com/pro-saas-starter-kit"
+            >
+              Next.js Boilerplate SaaS
+            </a>
+          ),
+        })}
+      </p>
       <Sponsors />
     </>
   );
