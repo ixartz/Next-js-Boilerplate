b82f77387b144ddd4716bd8749560a92d3563e1e
Rémi Weng <ixartz@users.noreply.github.com>
2025-03-26 23:30:20 +0100

    fix: replace Env.ARCJET_KEY with process.env.ARCJET_KEY in libs/Arcjet.ts to reduce middlware size

--- src/libs/Arcjet.ts
+++ src/libs/Arcjet.ts
@@ -1,10 +1,9 @@
 import arcjet, { shield } from '@arcjet/next';
-import { Env } from './Env';
 
 // Create a base Arcjet instance which can be imported and extended in each route.
 export default arcjet({
   // Get your site key from https://launch.arcjet.com/Q6eLbRE
-  key: Env.ARCJET_KEY!,
+  key: process.env.ARCJET_KEY || '',
   // Identify the user by their IP address
   characteristics: ['ip.src'],
   rules: [
