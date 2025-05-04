ab0d956db1dde3150e8eb0f67297e81060ee1265
Rémi Weng <ixartz@users.noreply.github.com>
2025-05-04 22:19:43 +0200

    refactor: simplify middleware config matcher for clarity

--- src/middleware.ts
+++ src/middleware.ts
@@ -71,10 +71,8 @@ export default async function middleware(
 }
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|monitoring|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: '/((?!api|trpc|_next|_vercel|monitoring|.*\\..*).*)',
 };
