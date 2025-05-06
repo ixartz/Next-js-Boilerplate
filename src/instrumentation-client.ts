79635655cb3e09494689fcddfe852f90faa3cad1
Rémi Weng <ixartz@users.noreply.github.com>
2025-05-06 21:10:22 +0200

    fix: in instrumentation client, add environment variable to disable sentry

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,34 +5,36 @@
 import * as Sentry from '@sentry/nextjs';
 import * as Spotlight from '@spotlightjs/spotlight';
 
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
+  Sentry.init({
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+    // Add optional integrations for additional features
+    integrations: [
+      Sentry.replayIntegration(),
+    ],
 
-  // Adds request headers and IP for users, for more info visit
-  sendDefaultPii: true,
+    // Adds request headers and IP for users, for more info visit
+    sendDefaultPii: true,
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // Define how likely Replay events are sampled.
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
 
-if (process.env.NODE_ENV === 'development') {
-  Spotlight.init();
+  if (process.env.NODE_ENV === 'development') {
+    Spotlight.init();
+  }
 }
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
