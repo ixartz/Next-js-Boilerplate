e626bab6d5574a6f65dbdf0f05825fd010233a21
Rémi Weng <ixartz@users.noreply.github.com>
2025-05-07 02:19:58 +0200

    refactor: remove unnecessary blank line in Sentry client configuration

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,7 +1,6 @@
 // This file configures the initialization of Sentry on the client.
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs';
 import * as Spotlight from '@spotlightjs/spotlight';
 
