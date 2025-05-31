b1381b1666bdb3d53e45779183fa2fbf9f094f8f
Rémi Weng <ixartz@users.noreply.github.com>
2025-05-31 04:57:42 +0200

    fix: update type definitions for next-intl module

--- src/types/global.d.ts
+++
@@ -1,5 +0,0 @@
-// Use type safe message keys with `next-intl`
-type Messages = typeof import('../locales/en.json');
-
-// eslint-disable-next-line
-declare interface IntlMessages extends Messages {}
