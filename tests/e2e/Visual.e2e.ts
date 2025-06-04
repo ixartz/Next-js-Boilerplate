d97076fb4a263431faf52bdcd3cf20a457c216be
Rémi Weng <ixartz@users.noreply.github.com>
2025-06-05 01:42:36 +0200

    ci: make sure all resources has loaded before taking screenshot

--- tests/e2e/Visual.e2e.ts
+++ tests/e2e/Visual.e2e.ts
@@ -1,9 +1,10 @@
+/* eslint-disable playwright/no-networkidle */ // Ensure all network requests are completed before taking a snapshot
 import { expect, takeSnapshot, test } from '@chromatic-com/playwright';
 
 test.describe('Visual testing', () => {
   test.describe('Static pages', () => {
     test('should take screenshot of the homepage', async ({ page }, testInfo) => {
-      await page.goto('/');
+      await page.goto('/', { waitUntil: 'networkidle' });
 
       await expect(
         page.getByRole('heading', { name: 'Boilerplate Code for Your Next.js Project with Tailwind CSS' }),
@@ -13,7 +14,7 @@ test.describe('Visual testing', () => {
     });
 
     test('should take screenshot of the about page', async ({ page }, testInfo) => {
-      await page.goto('/about');
+      await page.goto('/about', { waitUntil: 'networkidle' });
 
       await expect(
         page.getByRole('link', { name: 'About' }),
@@ -23,7 +24,7 @@ test.describe('Visual testing', () => {
     });
 
     test('should take screenshot of the portfolio page', async ({ page }, testInfo) => {
-      await page.goto('/portfolio');
+      await page.goto('/portfolio', { waitUntil: 'networkidle' });
 
       await expect(
         page.getByText('Welcome to my portfolio page!'),
@@ -41,11 +42,14 @@ test.describe('Visual testing', () => {
         page.getByText('Created a set of promotional'),
       ).toBeVisible();
 
+      // Ensure all network requests are completed before taking a snapshot
+      await page.waitForLoadState('networkidle');
+
       await takeSnapshot(page, testInfo);
     });
 
     test('should take screenshot of the French homepage', async ({ page }, testInfo) => {
-      await page.goto('/fr');
+      await page.goto('/fr', { waitUntil: 'networkidle' });
 
       await expect(
         page.getByRole('heading', { name: 'Code de démarrage pour Next.js avec Tailwind CSS' }),
