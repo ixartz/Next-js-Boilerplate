207b82a7894fc8ea9b328a7b3ab58d2fbd20c217
Rémi Weng <ixartz@users.noreply.github.com>
2025-06-10 23:16:32 +0200

    feat: start using pglite-socket instead of direct initialization in DB.ts

--- src/libs/DB.ts
+++ src/libs/DB.ts
@@ -1,43 +1,25 @@
-import type { PgliteDatabase } from 'drizzle-orm/pglite';
 import path from 'node:path';
-import { PGlite } from '@electric-sql/pglite';
 import { drizzle as drizzlePg } from 'drizzle-orm/node-postgres';
 import { migrate as migratePg } from 'drizzle-orm/node-postgres/migrator';
-import { drizzle as drizzlePglite } from 'drizzle-orm/pglite';
-import { migrate as migratePglite } from 'drizzle-orm/pglite/migrator';
-import { PHASE_PRODUCTION_BUILD } from 'next/dist/shared/lib/constants';
-import { Client } from 'pg';
 import * as schema from '@/models/Schema';
 import { Env } from './Env';
 
-let client;
-let drizzle;
+// Stores the db connection in the global scope to prevent multiple instances due to hot reloading with Next.js
+const global = globalThis as unknown as { client: any; drizzle: any };
 
-if (process.env.NEXT_PHASE !== PHASE_PRODUCTION_BUILD && Env.DATABASE_URL) {
-  client = new Client({
-    connectionString: Env.DATABASE_URL,
-  });
-  await client.connect();
-
-  drizzle = drizzlePg(client, { schema });
-  await migratePg(drizzle, {
-    migrationsFolder: path.join(process.cwd(), 'migrations'),
-  });
-} else {
-  // Stores the db connection in the global scope to prevent multiple instances due to hot reloading with Next.js
-  const global = globalThis as unknown as { client: PGlite; drizzle: PgliteDatabase<typeof schema> };
-
-  if (!global.client) {
-    global.client = new PGlite();
-    await global.client.waitReady;
-
-    global.drizzle = drizzlePglite(global.client, { schema });
-  }
-
-  drizzle = global.drizzle;
-  await migratePglite(global.drizzle, {
-    migrationsFolder: path.join(process.cwd(), 'migrations'),
+if (!global.drizzle) {
+  global.drizzle = drizzlePg({
+    connection: {
+      connectionString: Env.DATABASE_URL,
+      ssl: false, // Set to true if your database requires SSL
+    },
+    schema,
   });
 }
+
+const drizzle = global.drizzle;
+await migratePg(global.drizzle, {
+  migrationsFolder: path.join(process.cwd(), 'migrations'),
+});
 
 export const db = drizzle;
