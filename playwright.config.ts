e582d311f5b8f37ac5a04d149a727209bab614b9
Rémi Weng <ixartz@users.noreply.github.com>
2025-06-12 16:46:56 +0200

    fix: before running e2e in CI, make sure the db is initilized correctly

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig<ChromaticConfig>({
   // Run your local dev server before starting the tests:
   // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
-    command: process.env.CI ? 'npx run-p db-server:memory start' : 'npx run-p db-server:memory dev:next',
+    command: process.env.CI ? 'npx pglite-server --run "npm run start"' : 'npx run-p db-server:memory dev:next',
     url: baseURL,
     timeout: 2 * 60 * 1000,
     reuseExistingServer: !process.env.CI,
