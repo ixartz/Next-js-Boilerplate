d2db823114dc6f0ab22fee7310f13762a2cbca25
Rémi Weng <ixartz@users.noreply.github.com>
2026-02-04 20:37:02 +0100

    fix: enable react compiler only in production environment to keep dev env fast

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const baseConfig: NextConfig = {
   },
   poweredByHeader: false,
   reactStrictMode: true,
-  reactCompiler: true,
+  reactCompiler: process.env.NODE_ENV === 'production', // Keep the development environment fast
   outputFileTracingIncludes: {
     '/': ['./migrations/**/*'],
   },
