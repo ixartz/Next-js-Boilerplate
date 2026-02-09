84fb14855614cec8d283b7bf8be745027eae97da
Rémi Weng <ixartz@users.noreply.github.com>
2026-02-09 02:21:29 +0100

    chore: disable warning for incompatible library in react hooks raised by ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -59,6 +59,7 @@ export default antfu(
       'style/brace-style': ['error', '1tbs'], // Use the default brace style
       'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
       'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+      'react-hooks/incompatible-library': 'off', // Disable warning for compilation skipped
       'node/prefer-global/process': 'off', // Allow using `process.env`
       'test/padding-around-all': 'error', // Add padding in test files
       'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
