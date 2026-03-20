8f819406ce77d01506eea2d1a9a046057be504da
Rémi Weng <ixartz@users.noreply.github.com>
2026-03-20 20:26:53 +0100

    test: remove outdated Checkly URL note from sanity e2e test

--- tests/e2e/Sanity.check.e2e.ts
+++ tests/e2e/Sanity.check.e2e.ts
@@ -7,11 +7,6 @@ import { expect, test } from '@playwright/test';
 // On the other hand, E2E tests ending with `*.e2e.ts` are only run before deployment.
 // You can run them locally or on CI to ensure that the application is ready for deployment.
 
-// BaseURL needs to be explicitly defined in the test file.
-// Otherwise, Checkly runtime will throw an exception: `CHECKLY_INVALID_URL: Only URL's that start with http(s)`
-// You can't use `goto` function directly with a relative path like with other *.e2e.ts tests.
-// Check the example at https://feedback.checklyhq.com/changelog/new-changelog-436
-
 test.describe('Sanity', () => {
   test.describe('Static pages', () => {
     test('should display the homepage', async ({ page }) => {
