e092ad1e0d96e758bb80eae9ca61e76d88e5b949
Huy Thach <tnyh02@gmail.com>
2026-04-21 18:17:56 +0700

    chore: set generic app name in AppConfig

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -7,7 +7,7 @@ const localePrefix: LocalePrefixMode = 'as-needed';
 
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
-  name: 'Nextjs Starter',
+  name: 'My App',
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
