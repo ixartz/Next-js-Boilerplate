4c4f59b8c56281b0d0c397454b914ea4da323b82
Huy Thach <tnyh02@gmail.com>
2026-04-21 18:17:51 +0700

    chore: clear database schema, remove counter table

--- src/models/Schema.ts
+++ src/models/Schema.ts
@@ -1,24 +1,6 @@
-import { integer, pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
-
 // This file defines the structure of your database tables using the Drizzle ORM.
 
 // To modify the database schema:
 // 1. Update this file with your desired changes.
-// 2. Generate a new migration by running: `npm run db:generate`
-
-// The generated migration file will reflect your schema changes.
-// It automatically run the command `db-server:file`, which apply the migration before Next.js starts in development mode,
-// Alternatively, if your database is running, you can run `npm run db:migrate` and there is no need to restart the server.
-
-// Need a database for production? Check out https://get.neon.com/BMFYNtx
-// Tested and compatible with Next.js Boilerplate
-
-export const counterSchema = pgTable('counter', {
-  id: serial('id').primaryKey(),
-  count: integer('count').default(0),
-  updatedAt: timestamp('updated_at', { mode: 'date' })
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
-  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
-});
+// 2. Generate a new migration by running: `bun run db:generate`
+// 3. Apply migrations: `bun run db:migrate`
