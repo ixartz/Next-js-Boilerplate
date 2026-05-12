1ceab583bf5e31573607d0f8e2affd9dd53a0b91
Rémi Weng <ixartz@users.noreply.github.com>
2026-05-12 02:08:34 +0200

    fix: exclude API routes from proxy matcher

--- src/proxy.ts
+++ src/proxy.ts
@@ -66,5 +66,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/_next`, `/_vercel` or `monitoring`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: '/((?!_next|_vercel|monitoring|.*\\..*).*)',
+  matcher: '/((?!_next|_vercel|monitoring|api|.*\\..*).*)',
 };
