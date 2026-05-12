bd495dcd9c2d9ad1650b5d468552e8667befb0b7
Rémi Weng <ixartz@users.noreply.github.com>
2026-05-12 02:32:35 +0200

    chore: update to the latest dependencies version

--- knip.config.ts
+++ knip.config.ts
@@ -2,16 +2,18 @@ import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   // Files to exclude from Knip analysis
-  ignore: ['checkly.config.ts', 'src/libs/I18n.ts', 'src/types/I18n.ts', 'tests/**/*.ts'],
+  ignore: ['checkly.config.ts', 'src/libs/I18n.ts', 'src/types/I18n.ts'],
   // Dependencies to ignore during analysis
   ignoreDependencies: [
     '@commitlint/types',
     '@clerk/shared',
-    '@faker-js/faker',
     '@swc/helpers', // Avoid error in CI: "`npm ci` can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync."
-    'postcss',
     'vite',
   ],
+  // Include custom Playwright test file suffixes
+  playwright: {
+    entry: ['tests/**/*.@(integ|e2e).ts'],
+  },
   // Binaries to ignore during analysis
   ignoreBinaries: [
     'production', // False positive raised with dotenv-cli
@@ -19,6 +21,7 @@ const config: KnipConfig = {
   compilers: {
     css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/g)].join('\n'),
   },
+  treatConfigHintsAsErrors: true,
 };
 
 export default config;
